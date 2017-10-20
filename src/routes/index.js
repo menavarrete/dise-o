7cdb0ed51f11a9fce265b97db03abe729bd85338
Thomas Pryce Jones <thompj94@gmail.com>
2017-10-19 21:34:01 -0300

    Index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,11 @@ const pkg = require('../../package.json');
 const router = new KoaRouter();
 
 router.get('/', async (ctx) => {
-  await ctx.render('index', { appVersion: pkg.version });
+  const candidates = await ctx.orm.Candidate.findAll();
+  await ctx.render('index', {
+    candidates,
+    candidatesPath: candidate => ctx.router.url('candidate', { id: candidate.id }),
+  });
 });
 
 module.exports = router;
